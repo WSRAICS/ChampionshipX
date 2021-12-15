efc1194bdf7e3bf03b7b78b0d3a7b8a5ada5358e
Haxman1337 <48284552+Haxman1337@users.noreply.github.com>
2021-12-15 14:56:06 +0300

    Initial commit

---
+++ WorldSkillsX/WorldSkillsX/Tiles/RoadSign.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using WorldSkillsX.Properties;
+
+
+namespace WorldSkillsX.Tiles
+{
+    public class RoadSign : Obstacle
+    {
+        Bitmap block = Resources.Block;
+        Bitmap heavy = Resources.HeavyBlock2;
+        Bitmap vagon = Resources.VagonBlock;
+
+        public RoadSign(int type)
+        {
+            ObstacleType = "rs";
+            Type = type;
+            pickImage();
+        }
+
+        private void pickImage()
+        {
+            if (Type == 0) Image = block;  //q
+            if (Type == 1) Image = heavy;  //w
+            if (Type == 2) Image = vagon;  //e
+        }
+
+        public void ChangeType(int type)
+        {
+            Type = type;
+            pickImage();
+        }
+
+        public new bool canGo(int i)
+        {
+            return true;
+        }
+    }
+}
