352cb86150f9506470175cd2a35245f68f443b4f
berserk1661 <96167992+berserk1661@users.noreply.github.com>
2021-12-15 14:40:33 +0300

    Add files via upload

---
+++ World Skills X/World Skills X/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace World_Skills_X
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int h = 20;
+        int a = 0;
+
+        List<string> ts = new List();
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void btnKrugX_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSeve_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ World Skills X/World Skills X/List.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace World_Skills_X
+{
+    internal class List : List<string>
+    {
+    }
+}
